7cc186931cae841750796b485ad93dc38c17dd2c
Walter Kachanovsky <walterkachanovsky@Walters-MacBook-Pro.local>
2017-08-29 14:32:39 +0300

    added rename functionality

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -15,20 +15,75 @@ class Chatbot extends Component {
         };
 
         this.duplicate = this.duplicate.bind(this);
+        this.rename = this.rename.bind(this);
+        this.saveName = this.saveName.bind(this);
+        this.cancelRename = this.cancelRename.bind(this);
+        this.cancelRenameAll = this.cancelRenameAll.bind(this);
     };
 
+
     duplicate(botId) {
+        this.cancelRenameAll();
+
+        const $bot = $($('.chatbot')[botId + 1]);
         const duplicated = {
-            img: $($('.chatbot')[botId + 1]).find('.bot-img img').attr('src'),
-            name: $($('.chatbot')[botId + 1]).find('.bot-name').text(),
-            nickname: $($('.chatbot')[botId + 1]).find('.bot-nickname').text(),
+            img: $bot.find('.bot-img img').attr('src'),
+            name: $bot.find('.bot-name').text(),
+            nickname: $bot.find('.bot-nickname').text(),
         };
-        this.props.sendBotData(duplicated)
+
+        this.props.sendBotData(duplicated);
     }
+
+
+    rename(botId) {
+        this.cancelRenameAll();
+
+        const $bot = $($('.chatbot')[botId + 1]);
+        const name = $bot.find('.bot-name').text();
+        const self = this;
+
+        $bot.find('.bot-info').addClass('hidden');
+        $bot.find('.rename-bot')
+            .val(name)
+            .keydown(function(e) {
+                if(e.which === 27) {
+                    self.cancelRename($bot);
+                }
+            })
+            .removeClass('hidden')
+            .focus();
+        $bot.find('.save-btn').removeClass('hidden');
+    }
+
+
+    cancelRename($bot) {
+        $bot.find('.rename-bot').addClass('hidden');
+        $bot.find('.save-btn').addClass('hidden');
+        $bot.find('.bot-info').removeClass('hidden');
+    }
+
+
+    cancelRenameAll() {
+        const self = this;
+
+        $('.chatbot').each(function () {
+            self.cancelRename($(this));
+        });
+    }
+
+
+    saveName() {
+        const $bot = $($('.chatbot')[this.props.id + 1]);
+        const newName = $bot.find('.rename-bot').val();
+        $bot.find('.bot-name').text(newName);
+
+        this.cancelRename($bot);
+    }
+
 
     render() {
         if (this.props.type === "add-new") {
-            console.log('add-new');
             return (
                 <div className="chatbot add-new">
                     <div className="inner">
@@ -50,9 +105,11 @@ class Chatbot extends Component {
                             <img src="images/link-icon.svg"/>
                         </a>
                     </div>
+                    <input type="text" name="rename-bot" className="rename-bot hidden"/>
                     <span className="settings-btn">
-                                    <CustomDropdown botId={this.props.id} duplicate={this.duplicate}/>
+                                    <CustomDropdown botId={this.props.id} duplicate={this.duplicate} rename={this.rename}/>
                             </span>
+                    <a className="save-btn hidden" onClick={this.saveName}>Save</a>
                 </div>
             </div>
         )
--- src/components/CustomDropdown.js
+++ src/components/CustomDropdown.js
@@ -4,7 +4,7 @@ import {ButtonToolbar, Dropdown, MenuItem} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import './CustomDropdown.css';
 
-const CustomDropdown = ({botId, duplicate}) => {
+const CustomDropdown = ({botId, duplicate, rename}) => {
 
     return (
         <ButtonToolbar>
@@ -14,7 +14,7 @@ const CustomDropdown = ({botId, duplicate}) => {
                 </Dropdown.Toggle>
                 <Dropdown.Menu className="super-colors">
                     <MenuItem eventKey="1" onClick={() => {duplicate(botId)}}>Duplicate</MenuItem>
-                    <MenuItem eventKey="2">Rename</MenuItem>
+                    <MenuItem eventKey="2" onClick={() => {rename(botId)}}>Rename</MenuItem>
                     <MenuItem eventKey="3">Delete</MenuItem>
                 </Dropdown.Menu>
             </Dropdown>
